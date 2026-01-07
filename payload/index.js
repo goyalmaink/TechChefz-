f999eb6e60cd067e8c586fb6721a1824bdc7cda6
GoelMaink <mayankgoel1421@gmail.com>
2026-01-07 21:23:22 +0530

    fix the string output

--- payload/index.js
+++ payload/index.js
@@ -1,5 +1,5 @@
 import xlsx from "xlsx";
-import fs from 'fs'
+import fs, { writeFileSync } from 'fs'
 const data = xlsx.readFile("platform.xlsx");
 const sheetName = data.SheetNames[0];
 const sheet = data.Sheets[sheetName];
@@ -18,13 +18,25 @@ const payload = excelData
         email: row["Primary Email"]
     }));
 
-fs.writeFileSync(
-    "payload.json",
-    JSON.stringify(payload, null, 2),
-    "utf-8"
-);
+// fs.writeFileSync("payload.json", JSON.stringify(payload, null, 2), "utf-8");
 
 console.log("Total Records:", payload.length);
-console.log("----------------------------")
 // console.log(payload);
-console.log(JSON.stringify(payload, null, 2));
+// console.log(JSON.stringify(payload, null, 2));
+// console.log(JSON.stringify(payload));
+
+// payload.forEach(item => {
+//   console.log(`"${item.code}" "${item.phone}"`);
+// });
+
+const result = payload.map(item => ({
+    code: `${item.code}`,
+    fname: item.fname ,
+    scname: item.scname ,
+    email: item.email ,
+    phone: `${item.phone}`
+}));
+
+console.log(JSON.stringify(result, null, 2));
+
+fs,writeFileSync("payload2.json",JSON.stringify(result, null, 2),"utf-8");
